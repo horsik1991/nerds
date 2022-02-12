4ca102398602526bf303ebb3c06d9a06e37c37a3
horsik1991 <horsik1991@gmail.com>
2022-02-12 11:59:28 +0500

    общие правки по стилям

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -2,6 +2,6 @@
 // Копируем все шрифты из папки dev в dist
 
 module.exports = function fonts() {
-  return $.main.src('dev/blocks/fonts/*.*')
+  return $.main.src('dev/static/fonts/*.*')
     .pipe($.main.dest('dist/static/fonts'))
 };
